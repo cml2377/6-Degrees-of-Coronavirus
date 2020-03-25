66d5c47e39eaa3f475a7f720fbd051cb402d537a
Crystal Ly <crystal.m.ly@gmail.com>
2020-03-25 17:38:39 -0500

    added home.js, added router to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
